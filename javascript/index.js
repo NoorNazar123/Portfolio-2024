42645898d0a593268656ef2bf6b0fa1df3aafb2d
NoorNazar123 <noor.nazar.programmer@gmail.com>
2024-04-19 19:41:45 +0500

    About section done

--- javascript/index.js
+++ javascript/index.js
@@ -8,3 +8,21 @@ hamburger.addEventListener("click", () => {
    navbarNav.classList.toggle("active");  
    console.log("ok");
 });
+
+// About sec
+const tablinks = document.getElementsByClassName("tab-links");
+const tabcontents = document.getElementsByClassName("tab-contents");
+function updateElem(elems,tabname) {
+  for(elem of elems){
+     if(elem.id == tabname ){
+        elem.classList.add(`${elem.classList[0]=="tab-links"? "active-link" : "active-tab"}`)
+     }else{
+        elem.classList.remove(`${elem.classList[0]=="tab-links"? "active-link" : "active-tab"}`)
+     }
+  }
+}
+function opentab(tabname) {
+   for(elems of [tablinks ,tabcontents]){
+      updateElem(elems,tabname)
+   }
+} 
